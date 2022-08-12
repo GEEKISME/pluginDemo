445cd49458d2e2df330ef7f75a432e0a5d41c3ef
GitHub Action <action@github.com>
2022-08-12 00:33:02 +0000

    Template cleanup

--- src/main/kotlin/com/github/geekisme/plugindemo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/geekisme/plugindemo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.geekisme.plugindemo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.geekisme.plugindemo.MyBundle
 
 class MyApplicationService {
 
